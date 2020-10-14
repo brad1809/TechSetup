4f66ff83d47171695faffebc89f28677675567db
Bradley Hodgkins <brad1809@hotmail.com>
2020-10-14 20:06:30 +0100

    Add validation

--- GoalsApp/ClientApp/src/Features/AddGoal.tsx
+++ GoalsApp/ClientApp/src/Features/AddGoal.tsx
@@ -1,5 +1,6 @@
 import { RouteComponentProps } from '@reach/router';
-import { Field, Form, Formik } from 'formik';
+import { Validator } from 'fluentvalidation-ts';
+import { ErrorMessage, Field, Form, Formik } from 'formik';
 import React from 'react';
 import { usePost } from '../Hooks/usePost';
 import { Goal } from './Goals';
@@ -14,6 +15,21 @@ const initialValues: FormModel = {
   name: '',
 };
 
+const maxNameLength = 20;
+class AddGoalValidator extends Validator<FormModel> {
+  constructor() {
+    super();
+
+    this.ruleFor('name').maxLength(maxNameLength).withMessage(`Pls no. Be shorter than ${maxNameLength}`);
+  }
+}
+
+const validate = (values: FormModel) => {
+  const validator = new AddGoalValidator();
+
+  return validator.validate(values);
+};
+
 export const AddGoal = (props: AddGoalProps) => {
   const { error, loading, onSubmit } = usePost<FormModel, Goal>('/api/goals', console.log);
 
@@ -26,10 +42,11 @@ export const AddGoal = (props: AddGoalProps) => {
   return (
     <div>
       <h1>Add Goal</h1>
-      <Formik initialValues={initialValues} onSubmit={onSubmit}>
+      <Formik initialValues={initialValues} onSubmit={onSubmit} validate={validate}>
         <Form>
           <label htmlFor='name'>Name</label>
           <Field name='name' type='text' />
+          <ErrorMessage name='name' />
           <button type='submit'>Submit</button>
         </Form>
       </Formik>
